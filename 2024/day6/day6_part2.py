ad292827873583cc0e912e07539340b11c094f2f
Renaud Lessard Larouche <sigmao@gmail.com>
2024-12-06 12:55:08 -0500

    Update day6_part2.py

--- 2024/day6/day6_part2.py
+++ 2024/day6/day6_part2.py
@@ -1,3 +1,4 @@
+import copy
 from pathlib import Path
 
 GUARD = "^"
@@ -5,6 +6,7 @@ WALL = "#"
 FLOOR = "."
 guard_orientation = 0  # 0 is up, 1 is right, 2 is down, 3 if left
 grid = Path("input.txt").read_text(encoding="utf-8").split("\n")
+grid = [list(row) for row in grid]
 
 # Get wall coordinates
 guard_x, guard_y = None, None
@@ -12,7 +14,7 @@ grid_height = len(grid)
 grid_width = len(grid[0])
 wall_coordinates = set()
 seen = set()
-next_tile: tuple[int, int] = (0, 0)
+next_tile = (0, 0)
 wall_candidates = set()
 
 # Conceptualize map
@@ -24,34 +26,65 @@ for y in range(grid_height):
         elif cell == GUARD:
             guard_x, guard_y = x, y
 
-while True:
-    if guard_orientation == 0:  # up
-        next_x, next_y = guard_x, guard_y - 1
-    elif guard_orientation == 1:  # right
-        next_x, next_y = guard_x + 1, guard_y
-    elif guard_orientation == 2:  # down
-        next_x, next_y = guard_x, guard_y + 1
-    else:  # left
-        next_x, next_y = guard_x - 1, guard_y
 
-    # Check bound
-    if next_x < 0 or next_x >= grid_width or next_y < 0 or next_y >= grid_height:
-        #seen.add((guard_x, guard_y))  # don't forget the last position
+def _get_next_position(x, y, orientation):
+    if orientation == 0:  # up
+        return x, y - 1
+    elif orientation == 1:  # right
+        return x + 1, y
+    elif orientation == 2:  # down
+        return x, y + 1
+    else:  # left
+        return x - 1, y
+
+
+def _is_out_of_bound(x, y):
+    return x < 0 or x >= grid_width or y < 0 or y >= grid_height
+
+
+def is_looping_if_adding_wall(x, y, orientation, wall_x, wall_y, seen):
+    global grid
+    grid[wall_y][wall_x] = WALL  # mutability for speed
+    seen = copy.copy(seen)
+    try:
+        while True:
+            next_x, next_y = _get_next_position(x, y, orientation)
+
+            # Check bound
+            if _is_out_of_bound(next_x, next_y):
+                return False
+
+            key = (next_x, next_y, orientation)
+            if key in seen:  # if we walk on the same path, we are looping
+                return True
+            seen.add(key)
+
+            if grid[next_y][next_x] == WALL:  # turn on wall
+                orientation = (orientation + 1) % 4
+            else:  # otherwise advance
+                x, y = next_x, next_y
+    finally:
+        grid[wall_y][wall_x] = FLOOR
+
+
+checked_wall_coordinates = set()
+while True:
+    next_x, next_y = _get_next_position(guard_x, guard_y, guard_orientation)
+
+    # Exit when out of bound
+    if _is_out_of_bound(next_x, next_y):
         break
 
     seen.add((guard_x, guard_y, guard_orientation))
 
-    next_orientation_if_wall = (guard_orientation + 1) % 5
-    if grid[next_y][next_x] == WALL:
-        guard_orientation = next_orientation_if_wall
+    if grid[next_y][next_x] == WALL:  # turn when facing a wall
+        guard_orientation = (guard_orientation + 1) % 4
     else:
-        # If it WAS a wall, would that result in a loop?
-        if (guard_x, guard_y, next_orientation_if_wall) in seen:
-            print(next_x+1, next_y+1)
-            wall_candidates.add((next_x, next_y))
+        if (next_x, next_y) not in checked_wall_coordinates:
+            checked_wall_coordinates.add((next_x, next_y))
+            if is_looping_if_adding_wall(guard_x, guard_y, guard_orientation, next_x, next_y, seen):
+                wall_candidates.add((next_x, next_y))
 
         guard_x, guard_y = next_x, next_y
 
 print(len(wall_candidates))
-# 227 is too low
-# 228 is too low
